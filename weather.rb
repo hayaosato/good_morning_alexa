6065ff188bd7826ec6bc796a0630dbe86dfa56b1
h-munesato <h-munesato@dip-net.co.jp>
2018-12-03 23:25:29 +0900

    get list

--- weather.rb
+++ weather.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/ruby
 require 'dotenv'
 require 'net/http'
+require 'json'
 
 def access_weather
   # https://api.openweathermap.org/data/2.5/forecast
@@ -9,12 +10,13 @@ def access_weather
   city = 'Tokyo,JP'
   api_key = ENV['WEATHER_KEY']
   access_url = base_url + '?q=' + city + '&' + 'appid=' + api_key
-  weather_data = Net::HTTP.get_print(URI.parse(access_url))
+  weather_data = Net::HTTP.get(URI.parse(access_url))
   parse_data(weather_data)
 end
 
 def parse_data(json_data)
-  puts json_data.class
+  hash_data = JSON.parse(json_data)
+  puts hash_data['list'][0]
 end
 
 Dotenv.load
