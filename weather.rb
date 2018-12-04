e25f4f4832b6cf9e7a422d95e0d7858478d6f834
hayaosato <jake.bibikyary.880@gmail.com>
2018-12-04 23:43:31 +0900

    get weather

--- weather.rb
+++ weather.rb
@@ -9,21 +9,26 @@ module WeatherReport
   def access_weather
     Dotenv.load
     base_url = 'https://api.openweathermap.org/data/2.5/forecast'
+    today = Time.now
+    @fixed_time = Time.local(today.year, today.month, '5', '18', '00', '00')
+    puts @fixed_time
     city = 'Tokyo,JP'
     api_key = ENV['WEATHER_KEY']
     access_url = base_url + '?q=' + city + '&' + 'appid=' + api_key
-    weather_data = Net::HTTP.get(URI.parse(access_url))
-    time = parse_data(weather_data)
-    puts time
+    @weather_data = Net::HTTP.get(URI.parse(access_url))
+    parse_data
+    @weather
   end
 
-  def parse_data(json_data)
-    hash_data = JSON.parse(json_data)
-    weather_datetime = hash_data['list'][0]['dt_txt']
-    utc_datetime = Time.strptime(weather_datetime + '+0000', '%Y-%m-%d %H:%M:%S %z')
-    utc_datetime.localtime
+  def parse_data
+    hash_data = JSON.parse(@weather_data)
+    hash_data['list'].each do |data|
+      weather_datetime = data['dt_txt']
+      utc_datetime = Time.strptime(weather_datetime + '+0000', '%Y-%m-%d %H:%M:%S %z')
+      if utc_datetime.localtime == @fixed_time
+        @weather = data['weather'][0]['main']
+        break
+      end
+    end
   end
 end
-
-include WeatherReport
-access_weather
